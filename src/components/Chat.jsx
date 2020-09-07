bba832bbef855029f22bfe9e78de25b2a39848d7
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-09-07 12:35:48 +0500

    Chat screen added

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  SearchOutlined,
+  AttachFile,
+  MoreVert,
+  InsertEmoticon,
+  Mic,
+} from "@material-ui/icons";
 
 function Chat() {
-  return <div className="chat">Chat</div>;
+  const [input, setInput] = useState("");
+
+  const sendMessage = (e) => {};
+  return (
+    <div className="chat">
+      {/* Header */}
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      {/* Chat body */}
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Makki</span>
+          This is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Makki</span>
+          This is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Makki</span>
+          This is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      {/* Chat footer */}
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            type="text"
+            placeholder="Type a message"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button type="submit" onClick={sendMessage}>
+            Send a message
+          </button>
+        </form>
+        <Mic />
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
